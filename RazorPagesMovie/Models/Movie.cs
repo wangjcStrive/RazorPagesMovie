252c4817499ef2082996e207f85f1efe7b93814f
jicwang <wangjc857@gmail.com>
2019-04-17 10:55:46 +0800

    Add project files.

---
+++ RazorPagesMovie/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMovie.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string MyProperty { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime dateTime { get; set; }
+        public string  Genre { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
